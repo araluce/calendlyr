c5320d25aa24b0a95bbf3f2e0ef06135157d901a
araluce <alvaro.fernandez@crisalix.com>
2021-09-21 18:15:46 +0200

    [FIX] Calendly::Object to_ostruct

--- lib/calendly/object.rb
+++ lib/calendly/object.rb
@@ -7,11 +7,9 @@ module Calendly
     end
 
     def to_ostruct(obj)
-      klass = obj.class
-      case klass
-      when Hash
+      if obj.is_a?(Hash)
         OpenStruct.new(obj.map { |key, val| [key, to_ostruct(val)] }.to_h)
-      when Array
+      elsif obj.is_a?(Array)
         obj.map { |o| to_ostruct(o) }
       else # Assumed to be a primitive value
         obj
