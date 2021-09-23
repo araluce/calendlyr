d6811e07b12c88f7d8f0ac126a43dabd3b64a509
araluce <alvaro.fernandez@crisalix.com>
2021-09-23 15:29:11 +0200

    Adding codecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require 'simplecov'
+SimpleCov.start
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 require "calendly"
 require "minitest/autorun"
 require "webmock/minitest"
