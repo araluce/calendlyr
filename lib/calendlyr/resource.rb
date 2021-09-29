4f49daa9d91bf4c082a2233717ca5fbf0ab919aa
araluce <alvaro.fernandez@crisalix.com>
2021-09-29 11:36:38 +0200

    [Refactor] Remove unnecessary methods

--- lib/calendlyr/resource.rb
+++ lib/calendlyr/resource.rb
@@ -21,14 +21,6 @@ module Calendlyr
       handle_response request(url, Net::HTTP::Post, body: body)
     end
 
-    def patch_request(url, body:)
-      handle_response request(url, Net::HTTP::Patch, body: body)
-    end
-
-    def put_request(url, body)
-      handle_response request(url, Net::HTTP::Put, body: body)
-    end
-
     def delete_request(url, params: {})
       handle_response request(url, Net::HTTP::Delete, params: params)
     end
