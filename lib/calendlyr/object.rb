1d0775bc68506b520c16b920077a092a76d144c8
araluce <alvaro.fernandez@crisalix.com>
2026-04-06 22:46:05 +0200

    feat: replace OpenStruct with hash-backed Object class
    OpenStruct is deprecated in Ruby 3.4+. Replace with a custom
    hash-backed class that preserves dynamic attribute access via
    method_missing/respond_to_missing?, recursive wrapping, deep
    to_h, inspect, == with proper hash/eql? contract.

--- lib/calendlyr/object.rb
+++ lib/calendlyr/object.rb
@@ -1,31 +1,94 @@
-require "ostruct"
-
 module Calendlyr
-  class Object < OpenStruct
+  class Object
     def self.get_slug(path)
       path.split("/").last
     end
 
-    def initialize(attributes)
-      super(to_ostruct(attributes.merge(uuid: extract_uuid(attributes))))
-    end
+    private_class_method :get_slug
 
-    def to_ostruct(obj)
-      if obj.is_a?(Hash)
-        OpenStruct.new(obj.map { |key, val| [key, to_ostruct(val)] }.to_h)
-      elsif obj.is_a?(Array)
-        obj.map { |o| to_ostruct(o) }
-      else # Assumed to be a primitive value
-        obj
+    def initialize(attributes = nil, add_uuid: true, **kwargs)
+      attrs = (attributes || {}).merge(kwargs).dup
+      if add_uuid && !attrs.key?(:uuid) && !attrs.key?("uuid")
+        attrs = attrs.merge(uuid: extract_uuid(attrs))
+      end
+
+      @attributes = attrs.each_with_object({}) do |(key, value), hash|
+        hash[key.to_s] = wrap(value)
       end
     end
 
-    def extract_uuid(attrs)
-      attrs["uri"] ? get_slug(attrs["uri"]) : nil
+    def method_missing(name, *args, &block)
+      if args.empty? && block.nil?
+        return @attributes[name.to_s] if @attributes.key?(name.to_s)
+
+        return nil
+      end
+
+      super
     end
 
+    def respond_to_missing?(name, include_private = false)
+      @attributes.key?(name.to_s) || super
+    end
+
+    def to_h
+      @attributes.each_with_object({}) do |(key, value), hash|
+        hash[key.to_sym] = unwrap(value)
+      end
+    end
+
+    def inspect
+      attributes = @attributes.map do |key, value|
+        "#{key}=#{value.inspect}"
+      end.join(" ")
+      "#<#{self.class} #{attributes}>"
+    end
+
+    def ==(other)
+      other.is_a?(self.class) && to_h == other.to_h
+    end
+
+    def eql?(other)
+      self == other
+    end
+
+    def hash
+      to_h.hash
+    end
+
+    private
+
+    def extract_uuid(attrs)
+      uri = attrs["uri"] || attrs[:uri]
+      uri ? get_slug(uri) : nil
+    end
+
+    protected
+
     def get_slug(path)
-      Calendlyr::Object.get_slug(path)
+      self.class.send(:get_slug, path)
+    end
+
+    private
+
+    def wrap(value)
+      if value.is_a?(Hash)
+        self.class.new(value, add_uuid: false)
+      elsif value.is_a?(Array)
+        value.map { |item| wrap(item) }
+      else
+        value
+      end
+    end
+
+    def unwrap(value)
+      if value.is_a?(self.class)
+        value.to_h
+      elsif value.is_a?(Array)
+        value.map { |item| unwrap(item) }
+      else
+        value
+      end
     end
   end
 end
