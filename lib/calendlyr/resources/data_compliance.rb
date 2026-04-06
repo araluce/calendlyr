325eaf9fb5186dc628a5f0d9a949ae49cc476117
araluce <alvaro.fernandez@crisalix.com>
2026-04-06 23:34:41 +0200

    feat: add delete_scheduled_event_data endpoint
    Implement POST /data_compliance/deletion/events to remove
    scheduled events data within a time range. Includes test,
    fixture and documentation.

--- lib/calendlyr/resources/data_compliance.rb
+++ lib/calendlyr/resources/data_compliance.rb
@@ -3,5 +3,9 @@ module Calendlyr
     def delete_invitee_data(emails:)
       post_request("data_compliance/deletion/invitees", body: {emails: emails})
     end
+
+    def delete_scheduled_event_data(start_time:, end_time:)
+      post_request("data_compliance/deletion/events", body: {start_time: start_time, end_time: end_time})
+    end
   end
 end
